26228c2312ad3f9b002bc3966e95894e4efd7a74
Annette Odingo <odingoannette@gmail.com>
2019-11-12 18:36:10 +0300

    Revert "Finish Stretch"

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,31 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
-const MovieCard = ({movie}) => {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`/movies/${movie.id}`}>
-        <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-    </Link>
-    
-  );
+const MovieCard = props => {
+  return;
 };
 
 export default MovieCard;
-
-
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-import MovieCard from './MovieCard'
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -11,7 +9,6 @@ const MovieList = props => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovies(response.data);
-          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -24,12 +21,32 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
+function MovieDetails({ movie }) {
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
 
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default MovieList;
